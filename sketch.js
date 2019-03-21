66b12391d30b8f2a83fab6f5c35456c83f2cd4ba
Samuel Antonio Farach Rojas <farachsamuel@gmail.com>
2019-03-21 12:05:54 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+var objectCurrent;
+
+function setup() {
+	createCanvas(1200, 600, WEBGL);
+	rectMode(CENTER);
+	imageMode(CENTER);
+	//modelMode(CENTER);
+
+	//ship
+	shipPosition = new createVector(width / 2, height / 1.1);
+	objectCurrent = loadModel('https://github.com/SamuelFarach/P5-3D-Interaction/blob/master/assets/pyramid.obj');
+}
+
+function draw()
+{
+	background(210);
+
+	objectDisplay();
+}
+
+
+
+function objectDisplay()
+{
+	model(objectCurrent);
+}
